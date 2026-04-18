072aa6fd4d5ee6abe847c0c2ca0a6899922004f8
BigSmoke <cesarcadiz26@gmail.com>
2026-04-17 20:08:55 -0400

    feat: implementar obtenerHorarioPorCodigo en horarios service

--- src/services/horarios.service.js
+++ src/services/horarios.service.js
@@ -2,11 +2,15 @@ const horariosRepository = require("../db/horarios.repository");
 
 async function obtenerHorarios() {
   const horarios = await horariosRepository.getAllHorarios();
-
-  // aquí luego meteremos lógica TDD (filtrar, transformar, etc)
   return horarios;
 }
 
+async function obtenerHorarioPorCodigo(codigo) {
+  const horarios = await horariosRepository.getAllHorarios();
+  return horarios.find((horario) => horario.codigo === codigo);
+}
+
 module.exports = {
-  obtenerHorarios
+  obtenerHorarios,
+  obtenerHorarioPorCodigo
 };
