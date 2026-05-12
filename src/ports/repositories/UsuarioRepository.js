f0a1a324428a7e31cc5a2d0c220caf54db8ae7c4
BigSmoke <cesarcadiz26@gmail.com>
2026-05-12 09:59:21 -0400

    chore(hex): add Usuario domain entity and UsuarioRepository port

---
+++ src/ports/repositories/UsuarioRepository.js
@@ -0,0 +1,15 @@
+class UsuarioRepository {
+  async getAllUsuarios() {
+    throw new Error("Not implemented");
+  }
+
+  async getUsuarioByCorreo(correo) {
+    throw new Error("Not implemented");
+  }
+
+  async createUsuario(usuario) {
+    throw new Error("Not implemented");
+  }
+}
+
+module.exports = UsuarioRepository;
