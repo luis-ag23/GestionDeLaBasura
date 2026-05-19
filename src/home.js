7aa44e792a5d7ae937ebce1422357d0782b3060b
BigSmoke <cesarcadiz26@gmail.com>
2026-05-19 16:23:35 -0400

    agrega mapa real en home

--- src/home.js
+++ src/home.js
@@ -1,8 +1,10 @@
 const { cargarYRenderizarHorarios, enlazarFiltroDistrito } = require("./features/horarios/view/home.view");
+const { enlazarMapaZonas } = require("./features/mapa/view/homeMapa.view");
 
 async function iniciarHome() {
   try {
     console.log("Iniciando home...");
+    enlazarMapaZonas();
     await cargarYRenderizarHorarios("");
     enlazarFiltroDistrito();
     console.log("Home cargado correctamente");
