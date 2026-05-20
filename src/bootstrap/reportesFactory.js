44e39619270168b15e951eaec5d33694fb7197a7
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-05-20 14:21:54 -0400

    feature: edicion de reporte añadido a bootstrap

--- src/bootstrap/reportesFactory.js
+++ src/bootstrap/reportesFactory.js
@@ -1,23 +1,52 @@
 const reporteRepo = require("../adapters/persistence/pgReporteRepository");
 const reportePresenter = require("../adapters/presenter/jsonReportePresenter");
+
 const obtenerReportesUseCase = require("../application/reportes/obtenerReportes");
 const obtenerReportesPorUsuarioUseCase = require("../application/reportes/obtenerReportesPorUsuario");
 const crearReporteUseCase = require("../application/reportes/crearReporte");
+const editarReporteUseCase = require("../application/reportes/editarReporte");
 
 function obtenerReportes() {
-  return obtenerReportesUseCase({ reporteRepo, reportePresenter });
+  return obtenerReportesUseCase({
+    reporteRepo,
+    reportePresenter
+  });
 }
 
 function obtenerReportesPorUsuario(usuarioId) {
-  return obtenerReportesPorUsuarioUseCase({ reporteRepo, reportePresenter }, usuarioId);
+  return obtenerReportesPorUsuarioUseCase(
+    {
+      reporteRepo,
+      reportePresenter
+    },
+    usuarioId
+  );
 }
 
 function crearReporte(datos) {
-  return crearReporteUseCase({ reporteRepo, reportePresenter }, datos);
+  return crearReporteUseCase(
+    {
+      reporteRepo,
+      reportePresenter
+    },
+    datos
+  );
+}
+
+function editarReporte(id, datos) {
+  return editarReporteUseCase(
+    {
+      reporteRepo,
+      reportePresenter
+    },
+    id,
+    datos
+  );
 }
 
 module.exports = {
   obtenerReportes,
   obtenerReportesPorUsuario,
-  crearReporte
+  crearReporte,
+  editarReporte
 };
